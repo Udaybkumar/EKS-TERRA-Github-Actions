6d99d9218086e93c5df22df3bb50e0e004bcb7e9
Udaybkumar <udaybkumar031@gmail.com>
2026-01-29 15:57:34 +0530

    Added Defaults

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,48 +1,48 @@
-variable "aws-region" {}
-variable "env" {}
-variable "cluster-name" {}
-variable "vpc-cidr-block" {}
-variable "vpc-name" {}
-variable "igw-name" {}
-variable "pub-subnet-count" {}
+variable "aws-region" { default = "ap-southeast-1" }
+variable "env" {default = "dev"}
+variable "cluster-name" {default ="EKS-Cluster-${var.env}"}
+variable "vpc-cidr-block" {default = "10.0.0.0/24"}
+variable "vpc-name" {default = "EKS-VPC-${var.env}"}
+variable "igw-name" {default = "EKS-IGW-${var.env}"}
+variable "pub-subnet-count" {default = 2}
 variable "pub-cidr-block" {
   type = list(string)
 }
 variable "pub-availability-zone" {
   type = list(string)
 }
-variable "pub-sub-name" {}
-variable "pri-subnet-count" {}
+variable "pub-sub-name" {default = "EKS-Public-Subnet-${var.env}"}
+variable "pri-subnet-count" {default = 2}
 variable "pri-cidr-block" {
   type = list(string)
 }
 variable "pri-availability-zone" {
   type = list(string)
 }
-variable "pri-sub-name" {}
-variable "public-rt-name" {}
-variable "private-rt-name" {}
-variable "eip-name" {}
-variable "ngw-name" {}
-variable "eks-sg" {}
+variable "pri-sub-name" {default = "EKS-Private-Subnet-${var.env}"}
+variable "public-rt-name" {default = "EKS-Public-Route-Table-${var.env}"}
+variable "private-rt-name" {default = "EKS-Private-Route-Table-${var.env}"}
+variable "eip-name" {default = "EKS-EIP-${var.env}"}
+variable "ngw-name" {default = "EKS-NAT-Gateway-${var.env}"}
+variable "eks-sg" {default = "EKS-Security-Group-${var.env}"}
 
 
 # EKS
-variable "is-eks-cluster-enabled" {}
-variable "cluster-version" {}
-variable "endpoint-private-access" {}
-variable "endpoint-public-access" {}
+variable "is-eks-cluster-enabled" { default = true }
+variable "cluster-version" { default = "1.29" }
+variable "endpoint-private-access" { default = false }
+variable "endpoint-public-access" { default = true }
 variable "ondemand_instance_types" {
-  default = ["t3a.medium"]
+  default = ["t3.micro"]
 }
 
-variable "spot_instance_types" {}
-variable "desired_capacity_on_demand" {}
-variable "min_capacity_on_demand" {}
-variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
+variable "spot_instance_types" { default = ["t3.micro"]}
+variable "desired_capacity_on_demand" { default = 1 }
+variable "min_capacity_on_demand" { default = 1 }
+variable "max_capacity_on_demand" { default = 3 }
+variable "desired_capacity_spot" { default = 1 }
+variable "min_capacity_spot" { default = 1 }
+variable "max_capacity_spot" { default = 3 }
 variable "addons" {
   type = list(object({
     name    = string
--- module/variables.tf
+++ module/variables.tf
@@ -1,29 +1,29 @@
-variable "cluster-name" {}
-variable "cidr-block" {}
-variable "vpc-name" {}
-variable "env" {}
-variable "igw-name" {}
-variable "pub-subnet-count" {}
+variable "cluster-name" {default = "EKS-Cluster-${var.env}"}
+variable "cidr-block" {default = "10.0.0.0/24"}
+variable "vpc-name" {default = "EKS-VPC-${var.env}"}
+variable "env" {default = "dev"}
+variable "igw-name" {default = "EKS-IGW-${var.env}"}
+variable "pub-subnet-count" {default = 2}
 variable "pub-cidr-block" {
   type = list(string)
 }
 variable "pub-availability-zone" {
   type = list(string)
 }
-variable "pub-sub-name" {}
-variable "pri-subnet-count" {}
+variable "pub-sub-name" {default = "EKS-Public-Subnet-${var.env}"}
+variable "pri-subnet-count" {default = 2  }
 variable "pri-cidr-block" {
   type = list(string)
 }
 variable "pri-availability-zone" {
   type = list(string)
 }
-variable "pri-sub-name" {}
-variable "public-rt-name" {}
-variable "private-rt-name" {}
-variable "eip-name" {}
-variable "ngw-name" {}
-variable "eks-sg" {}
+variable "pri-sub-name" {default = "EKS-Private-Subnet-${var.env}"}
+variable "public-rt-name" {default = "EKS-Public-Route-Table-${var.env}"}
+variable "private-rt-name" {default = "EKS-Private-Route-Table-${var.env}"}
+variable "eip-name" {default = "EKS-EIP-${var.env}"}
+variable "ngw-name" {default = "EKS-NAT-Gateway-${var.env}"}
+variable "eks-sg" {default = "EKS-Security-Group-${var.env}"}
 
 #IAM
 variable "is_eks_role_enabled" {
@@ -34,21 +34,23 @@ variable "is_eks_nodegroup_role_enabled" {
 }
 
 # EKS
-variable "is-eks-cluster-enabled" {}
-variable "cluster-version" {}
-variable "endpoint-private-access" {}
-variable "endpoint-public-access" {}
+variable "is-eks-cluster-enabled" { default = true }
+variable "cluster-version" { default = "1.29" }
+variable "endpoint-private-access" { default = false }
+variable "endpoint-public-access" { default = true  }
 variable "addons" {
   type = list(object({
     name    = string
     version = string
   }))
 }
-variable "ondemand_instance_types" {}
-variable "spot_instance_types" {}
-variable "desired_capacity_on_demand" {}
-variable "min_capacity_on_demand" {}
-variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
+variable "ondemand_instance_types" {
+  default = ["t3.micro"]
+} 
+variable "spot_instance_types" { default = ["t3.micro"]}
+variable "desired_capacity_on_demand" { default = 1}
+variable "min_capacity_on_demand" { default = 1 }
+variable "max_capacity_on_demand" { default = 3 }
+variable "desired_capacity_spot" { default = 1}
+variable "min_capacity_spot" { default = 1 }
+variable "max_capacity_spot" { default = 3 }
