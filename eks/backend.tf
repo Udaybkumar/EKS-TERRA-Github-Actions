198f710b7448ff5446acb6dbc6736a8b3e89feb4
Udaybkumar <udaybkumar031@gmail.com>
2026-01-29 15:40:56 +0530

    Code ready to be deployed

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "dev-uday-terraform-state"
     region         = "ap-southeast-1"
-    key            = "eks/.terraform/terraform.tfstate"
+    key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
